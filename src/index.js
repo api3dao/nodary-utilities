53a6419c5bbe60e75bb3b324494145c1f0e1812c
bdrhn9 <bedirhan@api3.org>
2026-05-04 12:53:40 +0300

    Use @api3/contracts directly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const ethers = require("ethers");
 const airnodeAbi = require("@api3/airnode-abi");
-const { api3Contracts } = require("@api3/dapi-management");
+const api3Contracts = require("@api3/contracts");
 const { deriveWalletAddressFromSponsorAddress } = require("./airnode");
 const { nodaryAirnodeAddress, nodaryXPub } = require("../data/metadata.json");
 const nodaryEndpoints = require("../data/endpoints.json");
